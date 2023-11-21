79b7d9c5ad89f3b3faad8a2a94571900af002beb
Aalok-b098 <aalok0029bdeva@gmail.com>
2023-11-21 17:40:18 +0530

    feat:setup-css-library

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+     <h1 className="text-2xl font-bold mb-4 text-blue-500">hello tailwind css</h1>
+     
     </div>
   );
 }
