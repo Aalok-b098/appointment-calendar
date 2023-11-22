6a40dec8d9cc32296ba805057ff7e0182ecd46db
Aalok-b098 <aalok0029bdeva@gmail.com>
2023-11-22 13:29:16 +0530

    feat: added services for api's

---
+++ frontend/src/services/api.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const API_BASE_URL = '';
+
+export const getAppointmentService = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/events`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching calendar events');
+    }
+  };
+
+export const deleteAppointmentService = async (appointmentId) => {
+    try {
+      const response = await axios.delete(`${API_BASE_URL}/appointments/${appointmentId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error deleting appointment');
+    }
+  };
+
+export const addAppointmentService = async (eventData) => {
+      try {
+        const response = await axios.post(`${API_BASE_URL}/events`, eventData);
+        return response.data;
+      } catch (error) {
+        throw new Error('Error adding event to calendar');
+      }
+  };
+
+  export const updateAppointmentService = async (eventId, eventData) => {
+      try {
+        const response = await axios.put(`${API_BASE_URL}/events/${eventId}`, eventData);
+        return response.data;
+      } catch (error) {
+        throw new Error('Error updating event in calendar');
+      }
+  };
+
+  export const getUpcomingEvents = async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/api/events/upcoming`);
+      return response.data;
+    } catch (error) {
+      throw new Error('Error fetching upcoming events');
+    }
+  };
