27f7659d4758a92e09f3ee23cee092479420a59f
Aalok-b098 <aalok0029bdeva@gmail.com>
2023-11-22 12:26:06 +0530

    feat:added-drag-and-drop-functionality

--- frontend/src/components/CalendarView/Calendar.js
+++ frontend/src/components/CalendarView/Calendar.js
@@ -1,126 +1,143 @@
-import React,{ useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
-import 'react-big-calendar/lib/sass/styles.scss';
-import 'react-big-calendar/lib/addons/dragAndDrop/styles.scss';
-import AddEvent from './AddAppointment';
-import { Link } from 'react-router-dom';
-import EventCard from './AppointmentCard';
+import React, { useState, useRef } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import {Link} from "react-router-dom";
+
+const Calendar = () => {
+  const calendarComponentRef = useRef(null);
+
+  const [calendarWeekends, setCalendarWeekends] = useState(true);
+  
+    const [calendarEvents, setCalendarEvents] = useState([
+      {
+        id: 0,
+        title: "All Day Event very long title",
+        allDay: true,
+        start: new Date(2015, 3, 0),
+        end: new Date(2015, 3, 1)
+      },
+      {
+        id: 1,
+        title: "Long Event",
+        start: new Date(2023, 3, 7),
+        end: new Date(2023, 3, 10)
+      },
+      {
+        id: 2,
+        title: "Another Event",
+        start: new Date(2023, 3, 14),
+        end: new Date(2023, 3, 15)
+      },
+      {
+        id: 3,
+        title: "DTS ENDS",
+        start: new Date(2016, 10, 6, 0, 0, 0),
+        end: new Date(2016, 10, 13, 0, 0, 0)
+      },
+      {
+        id: 4,
+        title: "Some Event",
+        start: new Date(2015, 3, 9, 0, 0, 0),
+        end: new Date(2015, 3, 9, 0, 0, 0)
+      },
+      {
+        id: 5,
+        title: "Conference",
+        start: new Date(2015, 3, 11),
+        end: new Date(2015, 3, 13),
+        desc: "Big conference for important people"
+      },
+      {
+        id: 6,
+        title: "Meeting",
+        start: new Date(2015, 3, 12, 10, 30, 0, 0),
+        end: new Date(2015, 3, 12, 12, 30, 0, 0),
+        desc: "Pre-meeting meeting, to prepare for the meeting"
+      },
+      {
+        id: 7,
+        title: "Lunch",
+        start: new Date(2015, 3, 12, 12, 0, 0, 0),
+        end: new Date(2015, 3, 12, 13, 0, 0, 0),
+        desc: "Power lunch"
+      },
+      {
+        id: 8,
+        title: "Meeting",
+        start: new Date(2015, 3, 12, 14, 0, 0, 0),
+        end: new Date(2015, 3, 12, 15, 0, 0, 0)
+      },
+      {
+        id: 9,
+        title: "Happy Hour",
+        start: new Date(2015, 3, 12, 17, 0, 0, 0),
+        end: new Date(2015, 3, 12, 17, 30, 0, 0),
+        desc: "Most important meal of the day"
+      },
+      {
+        id: 10,
+        title: "Dinner",
+        start: new Date(2015, 3, 12, 20, 0, 0, 0),
+        end: new Date(2015, 3, 12, 21, 0, 0, 0)
+      },
+      {
+        id: 11,
+        title: "Birthday Party",
+        start: new Date(2015, 3, 13, 7, 0, 0),
+        end: new Date(2015, 3, 13, 10, 30, 0)
+      },
+      {
+        id: 12,
+        title: "Late Night Event",
+        start: new Date(2015, 3, 17, 19, 30, 0),
+        end: new Date(2015, 3, 18, 2, 0, 0)
+      },
+      {
+        id: 13,
+        title: "Multi-day Event",
+        start: new Date(new Date().setHours(new Date().getHours() + 3)),
+        end: new Date(new Date().setHours(new Date().getHours() + 3))
+      },
+      {
+        id: 14,
+        title: "Today",
+        start: new Date(new Date().setHours(new Date().getHours() + 3)),
+        end: new Date(new Date().setHours(new Date().getHours() + 3))
+      }
+    ]);
 
 
-const localizer = momentLocalizer(moment)
-
-const myEventsList = [
-  {
-    id: 0,
-    title: "All Day Event very long title",
-    allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1)
-  },
-  {
-    id: 1,
-    title: "Long Event",
-    start: new Date(2015, 3, 7),
-    end: new Date(2015, 3, 10)
-  },
-
-  {
-    id: 2,
-    title: "DTS STARTS",
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0)
-  },
-
-  {
-    id: 3,
-    title: "DTS ENDS",
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0)
-  },
-
-  {
-    id: 4,
-    title: "Some Event",
-    start: new Date(2015, 3, 9, 0, 0, 0),
-    end: new Date(2015, 3, 9, 0, 0, 0)
-  },
-  {
-    id: 5,
-    title: "Conference",
-    start: new Date(2015, 3, 11),
-    end: new Date(2015, 3, 13),
-    desc: "Big conference for important people"
-  },
-  {
-    id: 6,
-    title: "Meeting",
-    start: new Date(2015, 3, 12, 10, 30, 0, 0),
-    end: new Date(2015, 3, 12, 12, 30, 0, 0),
-    desc: "Pre-meeting meeting, to prepare for the meeting"
-  },
-  {
-    id: 7,
-    title: "Lunch",
-    start: new Date(2015, 3, 12, 12, 0, 0, 0),
-    end: new Date(2015, 3, 12, 13, 0, 0, 0),
-    desc: "Power lunch"
-  },
-  {
-    id: 8,
-    title: "Meeting",
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 15, 0, 0, 0)
-  },
-  {
-    id: 9,
-    title: "Happy Hour",
-    start: new Date(2015, 3, 12, 17, 0, 0, 0),
-    end: new Date(2015, 3, 12, 17, 30, 0, 0),
-    desc: "Most important meal of the day"
-  },
-  {
-    id: 10,
-    title: "Dinner",
-    start: new Date(2015, 3, 12, 20, 0, 0, 0),
-    end: new Date(2015, 3, 12, 21, 0, 0, 0)
-  },
-  {
-    id: 11,
-    title: "Birthday Party",
-    start: new Date(2015, 3, 13, 7, 0, 0),
-    end: new Date(2015, 3, 13, 10, 30, 0)
-  },
-  {
-    id: 12,
-    title: "Late Night Event",
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 18, 2, 0, 0)
-  },
-  {
-    id: 13,
-    title: "Multi-day Event",
-    start: new Date(2015, 3, 20, 19, 30, 0),
-    end: new Date(2015, 3, 22, 2, 0, 0)
-  },
-  {
-    id: 14,
-    title: "Today",
-    start: new Date(new Date().setHours(new Date().getHours() + 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3))
-  }
-];
-
-const AppointmentCalendar = () => {
-  const [selectedEvent, setSelectedEvent] = useState(null); 
-
-  const handleDelete = (eventId) => {
+  const toggleWeekends = () => {
+    setCalendarWeekends(!calendarWeekends);
   };
-  const handleUpdate = (eventId) => {
 
+  const gotoPast = () => {
+    if (calendarComponentRef.current) {
+      const calendarApi = calendarComponentRef.current.getApi();
+      calendarApi.gotoDate("2000-01-01");
+    }
   };
-  return(
-  <div>
+
+  const handleDateClick = (arg) => {
+    if (window.confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+      setCalendarEvents([...calendarEvents, { title: "New Event", start: arg.date, allDay: arg.allDay }]);
+    }
+  };
+
+  const handleEventDrop = (info) => {
+    const { start } = info.oldEvent._instance.range;
+
+    const { start: newStart } = info.event._instance.range;
+
+    if (new Date(start).getDate() === new Date(newStart).getDate()) {
+      info.revert();
+    }
+  };
+
+  return (
+    <>
     <div className='font-bold text-xl  my-3 mx-3'>Calendar</div>
     <div className="flex justify-end space-x-4 mx-3 my-3 ">
   <Link to="/addAppointment">
@@ -134,24 +151,37 @@ const AppointmentCalendar = () => {
     </button>
   </Link>
 </div>
+    <div className="demo-app">
+      <div className="demo-app-top">
+        <button onClick={toggleWeekends}>toggle weekends</button>&nbsp;
+        <button onClick={gotoPast}>go to a date in the past</button>
+        &nbsp; (Click week, add event and drag and drop event)
+      </div>
+      <div className="demo-app-calendar">
+        <FullCalendar
+          editable={true}
+          defaultView="timeGridWeek"
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay"
+          }}
+          header={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+          }}
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          ref={calendarComponentRef}
+          weekends={calendarWeekends}
+          events={calendarEvents}
+          dateClick={handleDateClick}
+          eventDrop={handleEventDrop}
+        />
+      </div>
+    </div>
+    </>
+  );
+};
 
-    <Calendar
-      localizer={localizer}
-      events={myEventsList}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 500 }}
-      components={{
-        event: ({ event }) => (
-          <EventCard
-            event={event}
-            onDelete={handleDelete}
-            onUpdate={handleUpdate}
-          />
-        ),
-      }}
-    />
-  </div>
-  )
-  }
-export default AppointmentCalendar;
+export default Calendar;
